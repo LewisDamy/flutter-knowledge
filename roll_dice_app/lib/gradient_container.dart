328c345f297d6b51807d8c9ff50aeead19e2b904
Lewis_Damy <ldamy16@gmail.com>
2025-07-24 12:29:33 -0300

    feat: add custom widget for contaner and background color customization

---
+++ roll_dice_app/lib/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  // Constructor function
+  const GradientContainer({super.key, required this.colors}); // Name option
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(child: DiceRoller()),
+    );
+  }
+}
